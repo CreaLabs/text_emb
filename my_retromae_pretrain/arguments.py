2c56fd2d392873acdab4b65eef1c7cc463789cd7
tyflow <jshi25@naver.com>
2024-06-14 14:24:03 +0900

    __post_init__

--- my_retromae_pretrain/arguments.py
+++ my_retromae_pretrain/arguments.py
@@ -21,9 +21,9 @@ class DataTrainingArguments:
     encoder_mlm_probability: float = field(default=0.3, metadata={"help": "mask ratio for encoder"})
     decoder_mlm_probability: float = field(default=0.5, metadata={"help": "mask ratio for decoder"})
 
-    def __post_init__(self):
-        if not os.path.exists(self.train_data):
-            raise FileNotFoundError(f"cannot find file: {self.train_data}, please set a true path")
+    # def __post_init__(self):
+    #     if not os.path.exists(self.train_data):
+    #         raise FileNotFoundError(f"cannot find file: {self.train_data}, please set a true path")
 
 
 @dataclass
